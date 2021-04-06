70454043917f007d2c1648b41f8b1cbb089b1b97
MahatKC <mkarvat@gmail.com>
2021-04-06 12:06:17 -0300

    busca em profundidade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import copy
 
 class Tree:
     # classe Tree cria um nó quando é chamada
@@ -35,39 +36,70 @@ def verifica_validade(x,y):
     else:
         return True
 
-#def cria_arvore():
+def cria_arvore(node_pai, indice_pai, nivel):
+    #a arvore é criada no nivel 1 e deve ter no máximo 8 níves. logo, se nivel==9, a criação da árvore deve cessar
+    if nivel==9:
+        return None
+    #verifica-se se o pai pode ter filhos. caso a posição de um nó seja inválida, tal nó é armazenado com um *, o que torna seu tipo de dado
+    #uma string.
+    if type(node_pai.data) != str:
+        #cria-se filhos para o nó pai atual com índices maiores que o do nó pai. Logo, a árvore é criada apenas com numeração ascendente
+        #Isto é: 0->10->25->...
+        #isso elimina a verificação repetida em combinações já testadas, como 0->25->10->...
+        for indice_atual in range(indice_pai+1,64):
+            #cria-se um nó vazio
+            node_atual = Tree(None)
+            #copia-se os nós do pai para o nó recém-criado
+            node_atual.parents=copy.deepcopy(node_pai.parents)
+            #adiciona-se o pai na lista de pais do nó atual
+            node_atual.parents.append(indice_pai)
+            valido=True
+            #verifica-se se a posição do nó atual é válida em relação a cada um dos nós pai que o precederam na árvore
+            for parent in node_atual.parents:
+                if not verifica_validade(parent,indice_atual):
+                    valido=False
+                    break
+            #caso o nó seja válido, adiciona-se o índice atual a ele
+            if valido:
+                node_atual.data = indice_atual
+            else:
+                node_atual.data=str(indice_atual)+"*"
+            #após a criação do nó, ele é adicionado a lista de filhos de seu pai
+            node_pai.children.append(node_atual)
 
-root = Tree(None)
-for i in range(64):
-    node = Tree(i)
+            #incrementa-se o contador de nível da árvore
+            nivel += 1
+            #chama-se a função de forma recursiva para criar o próximo nível da árvore a partir do nó atual
+            cria_arvore(node_atual, indice_atual, nivel)
+
+def inicia_arvore():
+    #visto que o primeiro nível da árvore não segue o mesmo padrão que os demais, ele é criado fora da função recursiva
+    raiz = Tree(None)
+    for i in range(64):
+        node = Tree(i)
+
+        raiz.children.append(node)
+
+        cria_arvore(node, i, 1)
     
-    for j in range(i+1, 64):
-        if verifica_validade(i,j):
-            node_filho = Tree(j)
-        else:
-            node_filho=Tree(str(j)+"*")
-        node_filho.parents.append(i)
+    return raiz
 
-        for k in range(j+1,64):
-            if type(node_filho.data) != str:
-                node_neto = Tree(None)
-                node_neto.parents=node_filho.parents
-                node_neto.parents.append(j)
-                valido=True
-                for parent in node_neto.parents:
-                    if not verifica_validade(parent,k):
-                        valido=False
-                        break
-                if valido:
-                    node_neto.data = k
-                else:
-                    node_neto.data=str(k)+"*"
-                node_filho.children.append(node_neto)
-               
+def busca_em_profundidade_preliminar(no_inicial):
+    pilha = []
+    busca_em_profundidade_recursivo(no_inicial, pilha)
 
+def busca_em_profundidade_recursivo(no_referencia, pilha):
+    pilha.append(no_referencia)
+    for no_filho in no_referencia.children:
+        busca_em_profundidade_recursivo(no_filho,pilha)
+    if len(pilha)>8:
+        print(f"Tamanho da pilha: {len(pilha)}")
+        pilha_para_printar = []
+        for nozinho in pilha[1:]:
+            pilha_para_printar.append(nozinho.data)
+        print(pilha_para_printar)
+        print("-"*10)
+    pilha.pop(pilha.index(no_referencia))
 
-        node.children.append(node_filho)
-
-    root.children.append(node)
-
-print(root.children[0].children[9])
+raiz = inicia_arvore()
+busca_em_profundidade_preliminar(raiz)
